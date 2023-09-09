0ce5e2d7b0e2b1438180891adf47b798b0187278
ttwange <ttwange2@gmail.com>
2023-09-09 07:45:44 +0300

    passing the dummy data as context

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render
 
 
-post = [
+posts = [  
   {
     "author": "saint"   
     "title": "My first blog post",
     "content":"This is my very first blog post, how exciting!"
   },
   {
-    "author": "saint"   
-    "title": "My first blog post",
-    "content":"This is my very first blog post, how exciting!"
+    "author": "she"   
+    "title": "My second blog post",
+    "content":"This is my second blog post, how exciting!"
   }
 ]
 
 
 # Create your views here.
 def home(request):
-  return render(request, 'blog/home.html')
+  context = {
+    'posts': posts  
+  }
+  return render(request, 'blog/home.html', context)
 
 def about(request):
   return render(request, 'blog/about.html')
