850112b566da1a0ebf4824d51431d797297934bf
ttwange <ttwange2@gmail.com>
2023-09-09 07:41:11 +0300

    Adding dummy data for testing

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+
+post = [
+  {
+    "author": "saint"   
+    "title": "My first blog post",
+    "content":"This is my very first blog post, how exciting!",
+
+  }
+]
+
+
 # Create your views here.
 def home(request):
   return render(request, 'blog/home.html')
