519a4f7e444a794475c56daaaacd402973a6a82e
ttwange <ttwange2@gmail.com>
2023-09-09 10:19:07 +0300

    Working on the post model

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [  
-  {
-    "author": "saint",   
-    "title": "My first blog post",
-    "content":"This is my very first blog post, how exciting!",
-    "date_posted":"September 9, 2023"
-  },
-  {
-    "author": "she",   
-    "title": "My second blog post",
-    "content":"This is my second blog post, how exciting!",
-    "date_posted":"September 9, 2023"
-  }
-]
-
 
 # Create your views here.
 def home(request):
