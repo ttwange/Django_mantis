1f8b18c29ae518c1f3f3efce83c9ff8455b3991e
ttwange <ttwange2@gmail.com>
2023-09-10 12:24:01 +0300

    Working on the profile page of the user

--- users/views.py
+++ users/views.py
@@ -16,3 +16,6 @@ def register(request):
       form = UserRegisterForm()
   context = {"form":form}
   return render(request,'users/register.html', context)
+
+def profile(request):
+   return render(request, 'users/profile.html')
