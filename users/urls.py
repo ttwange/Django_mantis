2c7672c95a38360bae4b1c3f93c7c1c488d66399
ttwange <ttwange2@gmail.com>
2023-09-10 08:06:19 +0300

    Added request validation

--- users/urls.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 # Create your views here.
 def register(request):
-  form = UserCreationForm()
+  if request.metthod == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      username = form.cleaned_data.get('username')
+      messages.success(request, f'Account created for {username}!')
+    else:
+      form = UserCreationForm()
   context = {"form":form}
   return render(request,'users/register.html', context)
