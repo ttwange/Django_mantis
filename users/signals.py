a15b6f6d01070709424bbd7b217021b0dcf01085
ttwange <ttwange2@gmail.com>
2023-09-11 07:53:37 +0300

    Working on implementing a user profile using signals

--- users/signals.py
+++ users/signals.py
@@ -11,6 +11,5 @@ def create_profile(sender, instance, created, **kwargs):
 
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-  if created:
-    Profile.objects.create(user=instance) 
+def save_profile(sender, instance, **kwargs):
+  instance.profile.save() 
