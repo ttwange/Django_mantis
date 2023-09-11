b429dd8263da43b5d7a5277f2a6db5bac34081e2
ttwange <ttwange2@gmail.com>
2023-09-11 06:36:35 +0300

    Fixed a bug in the Profile model

--- users/models.py
+++ users/models.py
@@ -6,5 +6,5 @@ class Profile(models.Model):
   user =  models.OneToOneField(User, on_delete=models.CASCADE)
   image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
-  def __init__(self):
+  def __str__(self):
     return f'{self.user.username} Profile'
